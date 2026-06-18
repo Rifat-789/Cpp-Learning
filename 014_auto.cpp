9d139a2dd0d8a1ccf174099f429b07399f431818
Rifat-789 <jahidulislamrefat6@gmail.com>
2026-06-18 15:09:57 +0600

    Learn about auto data type

---
+++ 014_auto.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+
+int main(){
+
+    auto var1 = 5;
+    auto var2 = 3.5;
+    auto var3 = 1.9f;
+    auto var4 = 'A';
+
+    std::cout << "Size of var1: " << sizeof(var1) << " bytes" << std::endl;
+    std::cout << "Size of var2: " << sizeof(var2) << " bytes" << std::endl;
+    std::cout << "Size of var3: " << sizeof(var3) << " bytes" << std::endl;
+    std::cout << "Size of var4: " << sizeof(var4) << " bytes" << std::endl;
+
+
+    return 0;
+}
