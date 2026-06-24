b6cce9d205aba3c6d177190e7028cbbdcb3ed841
Rifat-789 <jahidulislamrefat6@gmail.com>
2026-06-24 22:03:07 +0600

    Learn about Compound Assignment

---
+++ 019_compound_assignment.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+
+    int value = 5;
+
+    std::cout << "The value: " << value << std::endl;
+
+    value += 5;
+
+    std::cout << "The value: " << value << std::endl;
+
+    // The same things can be done with other operators just like in C
+
+
+
+    return 0;
+}
