f726de4b079d91850e2ebaaf22e8a9ed79a211fd
Rifat-789 <jahidulislamrefat6@gmail.com>
2026-06-28 11:01:41 +0600

    Learn about while loops in cpp

---
+++ 030_while_loops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+int main(){
+
+    const size_t count = 10;
+    size_t i = 0;
+
+    while (i < count){
+        std::cout << "I love C++" << std::endl;
+        i++;
+    }
+    
+    std::cout << "Loop done!" << std::endl;
+
+
+
+    return 0;
+}
